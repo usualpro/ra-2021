586d826abb67d7e42db8c25f46b9156e8f3c403f
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-10-11 21:26:47 +0200

    translation with react-intl

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,27 +1,38 @@
 import { useState } from 'react';
-
+import { IntlProvider, FormattedMessage } from 'react-intl'
 import { Paragraphe } from './Paragraphe';
-
 import { ParagrapheClass } from './ParagrapheClass';
 import './styles/main.scss';
-//import { ButtonRegular, ButtonRegularOutline } from './buttons/';
+import Fr from './translations/fr.json';
 
+const locale = navigator.language;
+const translationsEnum = {
+  "fr-FR": Fr
+};
 
 function App(props) {
   const [count, setCount] = useState(0);
   const onClickHandler = () => {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
   }
 
   return (
-    <div className="container">
-      <header className="App-header">
-        <h1>{props.text}</h1>
-        <Paragraphe  {...props} onClickHandler={onClickHandler} />
-        <ParagrapheClass />
-        {count}
-      </header>
-    </div>
+    <IntlProvider messages={translationsEnum[locale]} {...{ locale }} defaultLocale="en">
+      <div className="container">
+        <header className="App-header">
+          <h1>{props.text}</h1>
+          <Paragraphe  {...props} onClickHandler={onClickHandler} />
+          <ParagrapheClass />
+          learn react
+          {count}
+        </header>
+        <FormattedMessage
+          id="myMessage"
+          defaultMessage="Today is {ts, date, ::yyyyMMdd}"
+          values={{ ts: Date.now() }}
+        />
+      </div>
+    </IntlProvider>
   );
 }
 
