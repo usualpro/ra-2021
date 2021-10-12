274ad58dbac87ca6a7f968d73927dd54c5a24519
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-10-12 10:19:05 +0200

    add routing

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,27 +1,41 @@
-import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 
-import { Paragraphe } from './Paragraphe';
-
-import { ParagrapheClass } from './ParagrapheClass';
 import './styles/main.scss';
-//import { ButtonRegular, ButtonRegularOutline } from './buttons/';
+import { Home } from './pages/Home';
+import { Components } from './pages/Components';
 
 
-function App(props) {
-  const [count, setCount] = useState(0);
-  const onClickHandler = () => {
-    setCount(prevC => prevC + 1);
-  }
 
+
+const App = () => {
   return (
-    <div className="container">
-      <header className="App-header">
-        <h1>{props.text}</h1>
-        <Paragraphe  {...props} onClickHandler={onClickHandler} />
-        <ParagrapheClass />
-        {count}
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route path='/components'>
+            <Components />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <NavLink
+          exact={true}
+          className="btn btn-primary" to="/">
+          Home
+        </NavLink>&nbsp;
+        <NavLink
+          className="btn btn-primary"
+          to="/components">
+          Components
+        </NavLink>
+      </div>
+    </Router>
   );
 }
 
