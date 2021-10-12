0c062ce5f505a7538a36cc39142c4ed3370c3903
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-10-12 11:31:24 +0200

    import img

--- cra/src/pages/ClassVsFunctions.js
+++ cra/src/pages/ClassVsFunctions.js
@@ -1,1 +1,10 @@
-export const ClassVsFunctions = () => <>ClassVsFunctions</>;
+import MonImage from '../assets/componentbyclass.png';
+
+export const ClassVsFunctions = () => <>
+    <h1>Class vs functions</h1>
+    <h2>Functions</h2>
+    <img className="img-fluid w-100" src={process.env.PUBLIC_URL + '/assets/componentbyfunction.png'} alt="" />
+
+    <h2>Class</h2>
+    <img className="img-fluid w-100" src={MonImage} alt="" />
+</>;
