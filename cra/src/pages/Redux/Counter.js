d83531303f9b201232bdb841b4272a8aadddd1d8
Gary FELICITE-PRUDENT <gary.felicite-prudent@teladoc.com>
2021-10-13 14:20:22 +0200

    add redux 2 basecode

--- cra/src/pages/Redux/Counter.js
+++ cra/src/pages/Redux/Counter.js
@@ -1,18 +1,17 @@
-export const Counter = (props) => {
+export const Counter = props => {
     const { counter, increment, decrement, reset } = props;
     return <>
         <h1>Redux counter {counter}</h1>
-        <div className="d-grid gap-3">
-            <div>
-                <button className="btn btn-primary" onClick={increment}>+</button>
+        <div className="row">
+            <div className="col">
+                <button className="btn btn-primary w-100" onClick={increment}>INCREMENT BY 1</button>
             </div>
-            <div>
-                <button className="btn btn-secondary" onClick={decrement}>-</button>
+            <div className="col">
+                <button className="btn btn-secondary w-100" onClick={decrement}>DECREMENT BY 1</button>
             </div>
-            <div>
-                <button className="btn btn-danger" onClick={reset}>Reset</button>
+            <div className="col">
+                <button className="btn btn-danger w-100" onClick={reset}>RESET</button>
             </div>
         </div>
-
     </>
 };
